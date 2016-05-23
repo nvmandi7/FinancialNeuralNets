415830407f6b7c353ddda45ae77a4ed6a93a0e87
Shreyash Agrawal <shreyash@mit.edu>
2016-05-23 17:10:39 -0400

    Adding Previous Work

---
+++ finance_nn.py
@@ -0,0 +1,72 @@
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.optimizers import SGD
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.layers.embeddings import Embedding
+import retrieve_data
+
+
+
+test_days = 700
+
+X_train = normalized_data[:-test_days]
+X_test = normalized_data[-test_days:]
+
+y_train = labels[:-test_days]
+y_test = labels[-test_days:]
+
+print "Number of BUY Training Examples", y_train.count(1)
+print "Number of SELL Training Examples", y_train.count(0)
+print "Number of BUY Test Examples", y_test.count(1)
+print "Number of SELL Test Examples", y_test.count(0)
+num_classes = 2
+
+y_train = np_utils.to_categorical(y_train, num_classes) #Converts label data to a matrix
+
+#################################
+# Model specification
+# Start from an empty sequential model where we can stack layers
+model = Sequential()
+
+## Add a fully-connected layer.
+model.add(Dense(output_dim=10, input_dim=lookback_days))
+
+## Add tanh activation function to each neuron
+model.add(Activation("tanh"))
+
+## Add another fully-connected layer with 2 neurons, one for each class of labels
+model.add(Dense(output_dim=2, activation='softmax'))
+
+'''
+Trying Recurrent NN
+'''
+# model = Sequential()
+# model.add(Embedding(len(X_train), 128, input_length=lookback_days, dropout=0.2))
+# model.add(LSTM(128, dropout_W=0.2, dropout_U=0.2))
+# model.add(Dense(1))
+# model.add(Activation('sigmoid'))
+# model.compile(loss='binary_crossentropy',
+#               optimizer='adam',
+#               metrics=['accuracy'])
+# model.fit(X_train, y_train, nb_epoch=20, batch_size=2,validation_split=0.1, show_accuracy=True)
+# score, acc = model.evaluate(X_test, y_test,
+#                             batch_size=2)
+# print('Test score:', score)
+# print('Test accuracy:', acc)
+
+##################################
+
+## Compile the model with categorical_crossentrotry as the loss, and stochastic gradient descent (learning rate=0.001, momentum=0.5,as the optimizer)
+model.compile(loss='binary_crossentropy', optimizer=SGD(lr=0.01, momentum=0.5, nesterov=True), metrics=['accuracy'])
+
+## Fit the model (10% of training data used as validation set)
+model.fit(X_train, y_train, nb_epoch=10, batch_size=1,validation_split=0.1, show_accuracy=True)
+
+test_labels = model.predict_classes(X_test, batch_size=1,verbose=1)
+
+test_labels = list(test_labels)
+print test_labels.count(-1)
+print test_labels.count(0)
+print test_labels.count(1)
+
---
+++ retrieve_data.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import pandas.io.data as web
+from datetime import datetime
+
+start = datetime(2000,1,1)
+end = datetime(2016,4,20)
+
+#S&P Prediction
+stockRawData = web.DataReader("^GSPC", 'yahoo', start, end)
+num_rows = len(stockRawData.index)
+
+def create_lookback_returns_data(stock_data, num_days):
+	num_rows = len(stock_data.index)
+	stock_data_features = np.zeros([num_rows-num_days+1,num_days])
+	for day in xrange(0, num_rows-num_days+1):
+		for i in xrange(num_days):
+			new_day = day+i
+			adjustment_factor = float(stock_data['Adj Close'][new_day]/stock_data['Close'][new_day])
+			adjusted_open = adjustment_factor*stock_data['Open'][new_day]
+			stock_data_features[day,i] = 100.0*(stock_data['Adj Close'][new_day]-adjusted_open)/adjusted_open
+	return stock_data_features
+
+lookback_days = 5 #includes current day
+stock_data = create_lookback_returns_data(stockRawData, lookback_days)
+stock_data = np.delete(stock_data, -1, 0) #Remove last row since we can't label it
+
+'''
+Creating classification labels. 
+BUY or 1 if returns for next day are positive
+SELL or 0 otherwise
+'''
+def create_labels(stock_data, lookback_days):
+	num_rows = len(stock_data.index)
+	labels = []
+	for i in xrange(lookback_days-1, num_rows-1):
+		cur_day = stock_data.iloc[i+1]['Adj Close']
+		prev_day = stock_data.iloc[i]['Adj Close']
+		day_return  = 100.0*(cur_day-prev_day)/prev_day
+		if day_return < 0:
+			labels.append(0) #SELL
+			# stock_data.iloc[[i]] = -stock_data.iloc[[i]]
+		else:
+			labels.append(1) #BUY
+	return labels
+
+labels = create_labels(stockRawData, lookback_days)
+
+
+# combined = pd.concat([stockRawData,googStockData],axis=1)
+# stockRawData = stockRawData.drop(stockRawData.index[-1:])
+# stockRawData = stockRawData.iloc[np.random.permutation(len(stockRawData))]
+# data_to_process = stockRawData.values
+
+'''
+Normalizing the data using
+MAX-MIN Normalization in range [-1,1]
+'''
+def normalize_data(data):
+	num_cols = data.shape[1]
+	for i in xrange(num_cols):
+		col = data[:,i]
+		minimum = col.min()
+		maximum = col.max()
+		rangeOfCol = maximum-minimum
+		col = 2*(col-minimum)/(rangeOfCol)-1
+		data[:,i] = col
+	return data
+
+normalized_data = normalize_data(stock_data)
+
