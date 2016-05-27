b0ba91ca2677b7f553762b0414ff4f65370da931
Nathan Mandi <nvmandi7@berkeley.edu>
2016-05-27 12:29:35 -0400

    Proper table and csv save

--- correlation_test.py
+++ correlation_test.py
@@ -3,31 +3,42 @@ import pandas as pd
 #import pandas.io.data as web
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
-from datetime import datetime
+import datetime 
 
-start = datetime(2000,1,1)
-end = datetime(2016,5,20)
+start = datetime.datetime(2000,1,1)
+end = datetime.datetime(2016,5,20)
+plusDay = datetime.timedelta(days=1)
 
 stocks = ["^GSPC", "AAPL", "GE", "PG", "INTC", "TM", "KO", "JPM", "T"]
-
 correlation_factors = ["^BSESN", "^FCHI", "^FTSE", "CNY=X", "JPY=X", "GBP=X", "EUR=X"] #India, France, England, USD/CNH, USD/JPY, USD/GBP, USD/EUR
 
-#The stocks to check correlation with.
+# Stocks to check correlation with
 stock_data = web.DataReader(stocks, 'yahoo', start, end)["Adj Close"]
-print("Collected Stock Data")
-
+# Potential features
 factors = web.DataReader(correlation_factors, 'yahoo', start, end)["Adj Close"]
 
-for stock in stocks:
-	for factor in correlation_factors:
+# Consider changing to returns?
+
+
+corrs = np.ndarray((len(correlation_factors), len(stocks)))
+for i in range(len(stocks)):
+	for j in range(len(correlation_factors)):
+		factor = correlation_factors[j]
+		stock = stocks[i]
+
 		correlation_table = pd.concat([stock_data[stock], factors[factor]], axis=1)
 		correlation_table = correlation_table.dropna()
 
 		print("\n\n" + factor + " - " + stock)
-		print(correlation_table[stock].corr(correlation_table[factor]))
+		corrs[j][i] = correlation_table[stock].corr(correlation_table[factor])
+		print(corrs[j][i])
 		# print(correlation_table)
 		# correlation_table.plot.scatter(x=stock, y=factor)
 		# plt.show()
+data = np.hstack((np.array(correlation_factors).reshape((len(correlation_factors), 1)), corrs.astype(str)))
+np.savetxt('corr_table.csv', data, fmt='%s', delimiter=',', newline='\n', header='Stock, ' + ", ".join(stocks))
+
+
 
 # print(factors)
 # print(len(factors.index))
