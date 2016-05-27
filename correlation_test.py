ddbc4e08e92f08736cd6148a86df29a66c22da42
Shreyash Agrawal <shreyash@mit.edu>
2016-05-27 12:55:56 -0400

    Updated factors to include S&P

--- correlation_test.py
+++ correlation_test.py
@@ -12,7 +12,7 @@ day = datetime.timedelta(days=1)
 stocks = ["^GSPC", "AAPL", "GE", "PG", "INTC", "TM", "KO", "JPM", "T"] #The stocks to check correlation with.
 
 correlation_factors_no_offset = ["^SSEC", "^N225", "^BSESN"] #China, Japan, India
-correlation_factors_offset = ["^DJI", "^IXIC", "^GDAXI", "^FCHI", "^FTSE", "CNY=X", "JPY=X", "GBP=X", "EUR=X"] #DOW, Nasdaq, Germany, France, England, USD/CNY, USD/JPY, USD/GBP, USD/EUR
+correlation_factors_offset = ["^GSPC", "^DJI", "^IXIC", "^GDAXI", "^FCHI", "^FTSE", "CNY=X", "JPY=X", "GBP=X", "EUR=X"] #DOW, Nasdaq, Germany, France, England, USD/CNY, USD/JPY, USD/GBP, USD/EUR
 correlation_factors = correlation_factors_no_offset+correlation_factors_offset
 
 stock_data = web.DataReader(stocks, 'yahoo', start, end)["Adj Close"]
@@ -33,8 +33,13 @@ for i in range(len(stocks)):
 		factor = correlation_factors[j]
 		stock = stocks[i]
 
+
 		correlation_table = pd.concat([stock_data[stock], factors[factor]], axis=1)
 		correlation_table = correlation_table.dropna()
+		
+		if stock == factor:
+			corrs[j][i] = correlation_table.ix[:,0].corr(correlation_table.ix[:,1])
+			continue
 
 		print("\n\n" + factor + " - " + stock)
 		corrs[j][i] = correlation_table[stock].corr(correlation_table[factor])
@@ -43,7 +48,7 @@ for i in range(len(stocks)):
 		# correlation_table.plot.scatter(x=stock, y=factor)
 		# plt.show()
 data = np.hstack((np.array(correlation_factors).reshape((len(correlation_factors), 1)), corrs.astype(str)))
-np.savetxt('corr_table.csv', data, fmt='%s', delimiter=',', newline='\n', header='Stock, ' + ", ".join(stocks))
+np.savetxt('corr_table.csv', data, fmt='%s', delimiter=',', newline='\n', header='Features\Stocks, ' + ", ".join(stocks))
 
 
 # print(factors)
