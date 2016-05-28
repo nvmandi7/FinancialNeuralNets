2bacfd2de1860f7e2705032d034f515f535f8b80
Nathan Mandi <nvmandi7@berkeley.edu>
2016-05-28 19:52:35 -0400

    Feature progress

--- retrieve_data.py
+++ retrieve_data.py
@@ -7,8 +7,9 @@ import datetime
 # ------------------------------------------
 # Time Series Features
 
-start = datetime.datetime(2000,1,1)
+start = datetime.datetime(2004,1,1)
 end = datetime.datetime(2016,5,20)
+day = datetime.timedelta(days=1)
 
 #S&P Prediction
 stockRawData = web.DataReader("^GSPC", 'yahoo', start, end)
@@ -26,8 +27,8 @@ def create_lookback_returns_data(stock_data, num_days):
 	return stock_data_features
 
 lookback_days = 5 #includes current day
-stock_data = create_lookback_returns_data(stockRawData, lookback_days)
-stock_data = np.delete(stock_data, -1, 0) #Remove last row since we can't label it
+# stock_data = create_lookback_returns_data(stockRawData, lookback_days)
+# stock_data = np.delete(stock_data, -1, 0) #Remove last row since we can't label it
 
 '''
 Creating classification labels. 
@@ -71,14 +72,47 @@ def normalize_data(data):
 		data[:,i] = col
 	return data
 
-normalized_data = normalize_data(stock_data)
+# normalized_data = normalize_data(stock_data)
+
+sample = 'AAPL'
+sampleRawData = web.DataReader(sample, 'yahoo', start, end)
+
+
+# ------------------------------------------
+# Other Features
+
+correlation_factors_no_offset = ["^SSEC", "^N225", "^BSESN"] #China, Japan, India
+correlation_factors_offset = ["^GSPC", "^DJI", "^IXIC", "^GDAXI", "^FCHI", "^FTSE", "CNY=X", "JPY=X", "GBP=X", "EUR=X"] #DOW, Nasdaq, Germany, France, England, USD/CNY, USD/JPY, USD/GBP, USD/EUR
+correlation_factors = correlation_factors_no_offset+correlation_factors_offset
+
+
+factors_without_offset = web.DataReader(correlation_factors_no_offset, 'yahoo', start, end)["Adj Close"]
+factors_with_offset = web.DataReader(correlation_factors_offset, 'yahoo', start-day, end-day)["Adj Close"].shift(1)
+	
+
+factors = pd.concat([sampleRawData['Adj Close'], factors_without_offset, factors_with_offset], axis=1).dropna()
+factors = 100.0 * factors.diff() / factors.shift(1)
 
 # ------------------------------------------
 # Frequency Domain Features
-sample = 'AAPL'
-sampleRawData = web.DataReader(sample, 'yahoo', start, end)
+print(factors.head())
+sampleFinalStockData = factors['Adj Close']
 sampleDataTime = create_lookback_returns_data(sampleRawData, lookback_days)
 sampleDataFreq = np.fft.fft(sampleDataTime)
+
+###
+# Design matrix
+design = np.ndarray((len(factors['Adj Close']), len(factors) + 2*len(sampleDataTime)))
+design[:,len(factors)] = factors
+
+
+
+
+
+
+
+
+
 
 
 
