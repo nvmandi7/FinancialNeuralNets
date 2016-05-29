0c82b56385f4cdd9f98a58586a19b57ce47c0eeb
Nathan Mandi <nvmandi7@berkeley.edu>
2016-05-28 20:08:22 -0400

    same

--- retrieve_data.py
+++ retrieve_data.py
@@ -12,7 +12,7 @@ end = datetime.datetime(2016,5,20)
 day = datetime.timedelta(days=1)
 
 #S&P Prediction
-stockRawData = web.DataReader("^GSPC", 'yahoo', start, end)
+#stockRawData = web.DataReader("^GSPC", 'yahoo', start, end)
 
 correlation_factors_no_offset = ["^SSEC", "^N225", "^BSESN"] #China, Japan, India
 correlation_factors_offset = ["^GSPC", "^DJI", "^IXIC", "^GDAXI", "^FCHI", "^FTSE", "CNY=X", "JPY=X", "GBP=X", "EUR=X"] #DOW, Nasdaq, Germany, France, England, USD/CNY, USD/JPY, USD/GBP, USD/EUR
@@ -23,7 +23,7 @@ factors_with_offset = factors_with_offset.shift(1)
 factors = pd.concat([factors_without_offset, factors_with_offset], axis=1)
 factors = factors.diff()/factors.shift(1)*100
 
-num_rows = len(stockRawData.index)
+# num_rows = len(stockRawData.index)
 
 def create_lookback_returns_data(stock_data, num_days):
 	num_rows = len(stock_data.index)
@@ -59,7 +59,7 @@ def create_labels(stock_data, lookback_days):
 			labels.append(1) #BUY
 	return labels
 
-labels = create_labels(stockRawData, lookback_days)
+# labels = create_labels(stockRawData, lookback_days)
 
 
 # combined = pd.concat([stockRawData,googStockData],axis=1)
@@ -107,13 +107,13 @@ factors = 100.0 * factors.diff() / factors.shift(1)
 # Frequency Domain Features
 print(factors.head())
 sampleFinalStockData = factors['Adj Close']
-sampleDataTime = create_lookback_returns_data(sampleRawData, lookback_days)
-sampleDataFreq = np.fft.fft(sampleDataTime)
+sampleDataTime = create_lookback_returns_data(sampleFinalStockData, lookback_days)
+sampleDataFreq = np.absolute(np.fft.fft(sampleDataTime))
 
 ###
 # Design matrix
-design = np.ndarray((len(factors['Adj Close']), len(factors) + 2*len(sampleDataTime)))
-design[:,len(factors)] = factors
+design = np.hstack([factors, sampleDataTime, sampleDataFreq])
+
 
 
 
